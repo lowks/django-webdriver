595f932b4142b359c33645c4d4aa1d62c555bb27
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-04-15 10:29:04 +0200

    Bump to dev version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def README():
 
 setup(
     name='django-webdriver',
-    version='0.1.1',
+    version='0.1.2-dev',
     packages=find_packages(),
     include_package_data=True,
     license='Apache License 2.0',
